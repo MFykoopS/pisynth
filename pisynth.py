e3c8c1661a5d99a71738446aa25b472654896cc1
MFykoopS <SpookyFM@gmx.de>
2015-06-18 23:36:45 +0200

    cleanup

--- pisynth.py
+++ pisynth.py
@@ -7,10 +7,6 @@ from time import sleep
 
 running = True
 s = Server(duplex = 0)
-# Ich könnte versuchen, das 'audio'-Argument auf "offline" oder "offline_nb"
-# zu setzen und den output einfach auf aplay zu pipen. aplay hat auf dem Pi
-# funktioniert...
-# s.setOutputDevice(7) # muss angepasst werden!
 s.boot()
 pygame.init()
 window = pygame.display.set_mode((100, 100))
@@ -30,52 +26,48 @@ while running:
 			if event.key == K_a:
 				a.setFreq([261.63,261.63])
 				f.play()
-			if event.key == K_s:
+			elif event.key == K_s:
 				a.setFreq([293.66,293.66])
 				f.play()
-			if event.key == K_d:
+			elif event.key == K_d:
 				a.setFreq([329.63,329.63])
 				f.play()
-			if event.key == K_f:
+			elif event.key == K_f:
 				a.setFreq([349.23,349.23])
 				f.play()
-			if event.key == K_g:
+			elif event.key == K_g:
 				a.setFreq([392.00,392.00])
 				f.play()
-			if event.key == K_h:
+			elif event.key == K_h:
 				a.setFreq([440.00,440.00])
 				f.play()
-			if event.key == K_j:
+			elif event.key == K_j:
 				a.setFreq([493.88,493.88])
 				f.play()
-			if event.key == K_k:
+			elif event.key == K_k:
 				a.setFreq([523.25,523.25])
 				f.play()
+                        if pygame.key.get_mods() & KMOD_SHIFT:
+                                mult = 10
+                        else:
+                                mult = 1
 			if event.key == K_UP:
-				if pygame.key.get_mods() & KMOD_SHIFT:
-					mult = 10
-				else:
-					mult = 1
 				att += .01*mult
 				f.setAttack(att)
 				print "Attack:", att
-			if (event.key == K_DOWN and att > 0):
-				if pygame.key.get_mods() & KMOD_SHIFT:
-					mult = 10
-				else:
-					mult = 1
+			elif (event.key == K_DOWN and att > 0):
 				if att <= .01*mult:
 					att = 0
 				else:
 					att -= .01*mult
 				f.setAttack(att)
 				print "Attack:", att
-			if (event.key == K_0 and att > 0):
+			elif (event.key == K_0 and att > 0):
 				att = 0
 				print "Attack:", att
 			if event.key == K_ESCAPE:
 				running = False
-		if (event.type == KEYUP):
+		elif (event.type == KEYUP): # this is problematic!!
 		 	f.stop()
 	sleep(0.0001)
 pygame.quit()
